8f661eb39347c1e1d99b5ea85c9c6026f9dd031e
Krystal Amaia <krystalamaia@gmail.com>
2015-04-06 06:43:23 -0700

    Commented all my classes.  Carefish!  CAREFISH!!! CARRRRREEEFISCCCCSSSHHHHHHH

--- src/com/kamaia/cupsyballs/Main.java
+++ src/com/kamaia/cupsyballs/Main.java
@@ -5,14 +5,18 @@ import com.kamaia.cupsyballs.gui.menus.MainMenu;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Seriously, how the fuck am I supposed to comment this?
+		//There is like NOTHING HERE!
+
+		//Create A New Game Window
 		GameWindow gw = new GameWindow(80, 40);
+
+		//Create a new Main Menu, and pass it the game window to work with
 		MainMenu mm = new MainMenu(gw);
-		System.out.println("Game Running at: " + System.currentTimeMillis());
+
+		//see eminem run
 		mm.run();
 
 	}
